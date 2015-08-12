91d32abc49d988105b6fabc2f949fea80d61bd78
Vinoth Energetic <vinothcse2000@gmail.com>
2015-08-12 19:25:40 +0530

    Update post_or_pre_increment_operator_inside_size_of_operator.cpp

--- C-and-CPP/post_or_pre_increment_operator_inside_size_of_operator.cpp
+++ C-and-CPP/post_or_pre_increment_operator_inside_size_of_operator.cpp
@@ -1,1 +1,16 @@
 /* post or pre increment operator inside size of operator */
+#include<iostream>
+#include "exploreme.h"
+
+int main()
+{
+
+   int x=10;
+   
+   printf("\n  sizeof :: %u ", sizeof( x++ ) );  
+
+   printf("\n  x :: %d ", x ); 
+
+
+   return 0;
+}
