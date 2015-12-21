5feb14153037d431a0d76fddcd9d0a648b55d961
Vinoth Energetic <vinothcse2000@gmail.com>
2015-12-21 19:25:07 +0530

    Create int_to_float.cpp

---
+++ C-and-CPP/int_to_float.cpp
@@ -0,0 +1,28 @@
+#include <cmath>
+
+int main()
+{
+   using namespace std;
+
+   int unused = 70;
+
+   float f1 =  (unused/60.0) * 1;
+      printf("\n  :: %f ", f1  );
+
+
+
+
+
+   if(1)
+   {
+     f1= floor(f1);
+//      f1 =(int) f1;
+      printf("\n  :: %f ", f1  );
+   }
+
+
+
+
+
+   return 0;
+}
