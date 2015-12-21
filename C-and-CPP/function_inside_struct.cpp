307d9c3cd094cc3b47b1ca9fd93a5cd6a87eb2c4
Vinoth Energetic <vinothcse2000@gmail.com>
2015-12-21 19:21:57 +0530

    Create function_inside_struct.cpp

---
+++ C-and-CPP/function_inside_struct.cpp
@@ -0,0 +1,25 @@
+char *str;
+
+struct Assign
+{
+
+   /* This is not c code. C++ uses the struct as same as class
+    * In c, only pointer to function is allowed to declare inside the structure*/
+
+   Assign() { str = "Hello World!"; };
+   int abc()
+   {
+      int a;
+      return 0;
+
+   };
+};
+
+Assign oAssign;
+int main()
+{
+
+   return 0l;
+
+
+}
