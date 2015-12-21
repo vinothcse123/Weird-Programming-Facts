7f59411d1598f81a70b7e76b1a67023c77d541f6
Vinoth Energetic <vinothcse2000@gmail.com>
2015-12-21 19:16:37 +0530

    Create return_value_for_non_return_function.cpp

---
+++ C-and-CPP/return_value_for_non_return_function.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+
+int express()
+{
+   int a=5+5;
+}
+
+int donothing()
+{
+
+}
+
+int declarevariable()
+{
+   int a=9;
+}
+
+
+int main()
+{
+   using namespace std;
+
+   cout << '\n' << express();
+   cout << '\n' << donothing();
+   cout << '\n' << declarevariable();
+
+   return 0;
+}
+
