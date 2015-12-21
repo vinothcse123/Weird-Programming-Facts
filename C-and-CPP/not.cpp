c4f2e68fe04ce7affd084ca854b008ed7b3d8e19
Vinoth Energetic <vinothcse2000@gmail.com>
2015-12-21 19:35:52 +0530

    Create not.cpp

---
+++ C-and-CPP/not.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+int main()
+{
+   using namespace std;
+
+   printf("\n !0 :: %d ", !0 );
+   printf("\n !1 :: %d ", !1 );
+   printf("\n !5 :: %d ", !5 );
+   printf("\n !-5  :: %d ",!-4  );
+
+
+
+
+   return 0;
+}
