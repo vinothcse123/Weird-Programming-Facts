73fed8a133fd3a7945b000bf8b4eab6daa3384ea
Vinoth Energetic <vinothcse2000@gmail.com>
2015-12-21 19:24:39 +0530

    Create unsigned_long.c

---
+++ C-and-CPP/unsigned_long.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+
+   printf("\ndou  :: %d ", sizeof(double) );
+   printf("\n  :: %d ", sizeof(int) );
+   printf("\n  :: %d ", sizeof(long) );
+   printf("\n ");
+   printf("\n  :: %d ", sizeof(float) );
+
+
+
+   return 0;
+}
+
