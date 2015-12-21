ab0a73c66d2d0568913edc090566df251ec45249
Vinoth Energetic <vinothcse2000@gmail.com>
2015-12-21 19:39:14 +0530

    Create function_comma.cpp

---
+++ C-and-CPP/function_comma.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+void callme()
+{
+   printf("\n#######################################################\n");
+}
+
+void callmetoo()
+{
+   printf("\n$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$\n");
+}
+int main()
+{
+callme(),callmetoo();
+   return 0;
+}
