99a17323315eb9959e825bda810094be1ea58057
Vinoth Energetic <vinothcse2000@gmail.com>
2015-12-21 19:37:49 +0530

    Create char_to_int.cpp

---
+++ C-and-CPP/char_to_int.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+
+int main()
+{
+   using namespace std;
+
+   char s[10]="11";
+
+   char ss=1;
+   int i=2;
+    i=s[1]-0;
+    printf("\n  :: %c ",s[1]  );
+   printf("\n  :: %d ",i  );
+
+http://www.asciitable.com/;
+
+
+
+   return 0;
+}
