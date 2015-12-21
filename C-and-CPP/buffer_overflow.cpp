fec83c22fb435447ca163eca62e6db60ead177d8
Vinoth Energetic <vinothcse2000@gmail.com>
2015-12-21 19:24:30 +0530

    Create buffer_overflow.cpp

---
+++ C-and-CPP/buffer_overflow.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main()
+{
+   using namespace std;
+sleep(1);
+   int i=3000000424;
+   printf("\n  :: %d ", sizeof(int) );
+   printf("\n  :: %d ", i );
+
+
+
+   return 0;
+}
